3fe92bda94a9ce16a5e630ebe33d3e0e669a7108
Jay Bazuzi <jbazuzi@tableau.com>
2019-04-14 12:10:36 -0700

    R: extract property

--- Pipelines.Test/DotReporter.cs
+++ Pipelines.Test/DotReporter.cs
@@ -4,9 +4,11 @@ namespace Pipelines.Test
 {
     internal class DotReporter : GenericDiffReporter
     {
-        public DotReporter() : base(new DiffInfo(@"C:\Users\jbazuzi\AppData\Local\Programs\Microsoft VS Code\Code.exe",
+        public DotReporter() : base(new DiffInfo(VsCodeExePath,
             "-r {0}", () => new[] {"dot"}))
         {
         }
+
+        private static string VsCodeExePath => @"C:\Users\jbazuzi\AppData\Local\Programs\Microsoft VS Code\Code.exe";
     }
 }
