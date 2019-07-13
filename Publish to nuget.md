3c2d6cb5a044ba01eeea218d3905713275041537
Jay Bazuzi <jbazuzi@tableau.com>
2019-07-13 10:13:41 -0700

    Automate more of PublishToNuget

--- Publish to nuget.md
+++
@@ -1,5 +0,0 @@
-- [ ] Determine version number
-- [ ] Change version number in Directory.Build.props
-- [ ] delete `nuget_packages/` and rebuild
-- [ ] run `PublishToNuget.cmd`
-
--- PublishToNuget.cmd
+++ PublishToNuget.cmd
@@ -1,8 +1,17 @@
+::::::: Setup:
 :: 1. choco install nuget.commandline
-:: 2. NuGet.exe setapikey e39ea-get-the-full-key-on-nuget.org
-:: 3. increment version in Directory.Build.props
-:: 4. delete nuget_packages\
-:: 5. rebuild
+:: 2. Get API key from https://www.nuget.org/account/apikeys
+:: 3. NuGet.exe setapikey BLAH-BLAH-BLAH
+::
+:: Remember to update version number in Directory.Build.props before running this script
+
+IF EXIST nuget_packages (
+    DEL /S /Q nuget_packages\
+    IF ERRORLEVEL 1 exit /b 1
+)
+
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\\MSBuild\Current\Bin\MSBuild.exe" "/target:clean,restore,build
+IF ERRORLEVEL 1 exit /b 1
 
 NuGet push nuget_packages\Refactoring.Pipelines.?.?.?.nupkg -Source nuget.org
 NuGet push nuget_packages\Refactoring.Pipelines.Approvals.?.?.?.nupkg -Source nuget.org
