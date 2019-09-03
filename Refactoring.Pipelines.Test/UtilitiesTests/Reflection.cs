ec6d65437e1145a362dd1ec77717936cb77fb9b6
Jay Bazuzi <jbazuzi@tableau.com>
2019-09-03 07:15:28 -0700

    t Add test for ToReadableString

---
+++ Refactoring.Pipelines.Test/UtilitiesTests/Reflection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Refactoring.Pipelines.ReflectionUtilities;
+
+namespace Refactoring.Pipelines.Test.UtilitiesTests
+{
+    [TestClass]
+    public class Reflection
+    {
+        [TestMethod]
+        public void ToReadableString()
+        {
+            typeof(int).ToReadableString().Should().Be("Int32");
+            typeof(AClass).ToReadableString().Should().Be("AClass");
+            Tuple.Create(1, new AClass()).GetType().ToReadableString().Should().Be("Tuple<Int32, AClass>");
+        }
+
+        private class AClass
+        {
+        }
+    }
+}
