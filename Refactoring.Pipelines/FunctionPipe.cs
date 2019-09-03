2d2fd6ec4915f535312f818dd7a40845454840e7
Jay Bazuzi <jbazuzi@tableau.com>
2019-09-03 07:17:17 -0700

    r Move class to file

--- Refactoring.Pipelines/FunctionPipe.cs
+++ Refactoring.Pipelines/FunctionPipe.cs
@@ -46,26 +46,4 @@ namespace Refactoring.Pipelines
             return $@"{func.Method.DeclaringType.ToReadableString()}.{func.Method.Name}()";
         }
     }
-
-    internal class OutputNode : IGraphNode
-    {
-        private readonly string _name;
-        private readonly IGraphNode _owner;
-
-        public OutputNode(IGraphNode owner, string name)
-        {
-            _owner = owner;
-            _name = name;
-        }
-
-        string IGraphNode.Name =>
-            _name;
-
-        IEnumerable<IGraphNode> IGraphNode.Parents =>
-            new[] {_owner};
-
-        public override bool Equals(object other) { return other is OutputNode that && _owner == that._owner; }
-
-        public override int GetHashCode() { return 0; }
-    }
 }
