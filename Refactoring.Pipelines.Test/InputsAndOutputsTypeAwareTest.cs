51fd271811cf2a227bc31d9b8c9b53da4943bc8d
Jay Bazuzi <jbazuzi@tableau.com>
2019-09-07 11:55:12 -0700

    F .GetInputs<>().AndOutputs()

--- Refactoring.Pipelines.Test/InputsAndOutputsTypeAwareTest.cs
+++ Refactoring.Pipelines.Test/InputsAndOutputsTypeAwareTest.cs
@@ -17,20 +17,9 @@ namespace Refactoring.Pipelines.Test
             var middle = input.Process(p => p + 1);
             var end = middle.Process(p => p.ToString()).Collect();
 
+            var inputsAndOutputs = middle.GetInputs<int>().AndOutputs<string>();
+            var (inputPipe, collectorPipe) = inputsAndOutputs.AsTuple();
 
-            //var inputsAndOutputs = middle.GetInputsAndOutputs();
-            var inputsAndOutputs = middle.GetInputAndOutput<int, string>();
-            //var inputsAndOutputs = middle.Get2InputsAnd2Outputs<int, int, string, string>();
-
-
-            //InputsAndOutputs inputsAndOutputs = middle.GetInputs<int>().AndOutputs<string>();
-            //(InputPipe<int> inputPipe, CollectorPipe<string> collectorPipe) = inputsAndOutputs.Get();
-
-            //(InputPipe<int>  inputPipe, CollectorPipe<string> collectorPipe) = middle.GetInputs<int>().AndOutputs<string>();
-
-            //CollectorPipe<string> collectorPipe = /*(CollectorPipe<string>)*/inputsAndOutputs.Outputs.Single();
-            //InputPipe<int> inputPipe = (InputPipe<int>) inputsAndOutputs.Inputs.Single();
-            var (inputPipe, collectorPipe) = inputsAndOutputs.InputsAndOutputs;
             Assert.AreEqual(input, inputPipe);
             Assert.AreEqual(end, collectorPipe);
             inputsAndOutputs.Send(1);
@@ -49,12 +38,11 @@ namespace Refactoring.Pipelines.Test
 
             var inputsAndOutputs = InputsAndOutputsExtensions.GetInputsAndOutputs(joinedPipes);
 
-            ((InputPipe<int>)(inputsAndOutputs.Inputs[0])).Send(3);
-            ((InputPipe<int>)(inputsAndOutputs.Inputs[1])).Send(4);
+            ((InputPipe<int>) (inputsAndOutputs.Inputs[0])).Send(3);
+            ((InputPipe<int>) (inputsAndOutputs.Inputs[1])).Send(4);
 
-            Assert.AreEqual(7, ((CollectorPipe<int>)inputsAndOutputs.Outputs[0]).SingleResult);
-            Assert.AreEqual(12, ((CollectorPipe<int>)inputsAndOutputs.Outputs[1]).SingleResult);
+            Assert.AreEqual(7, ((CollectorPipe<int>) inputsAndOutputs.Outputs[0]).SingleResult);
+            Assert.AreEqual(12, ((CollectorPipe<int>) inputsAndOutputs.Outputs[1]).SingleResult);
         }
-
     }
 }
--- Refactoring.Pipelines/IntputsAndOutputs/InputsAndOutputsBasicTests - Copy.cs
+++ Refactoring.Pipelines/IntputsAndOutputs/InputsAndOutputsBasicTests - Copy.cs
@@ -5,9 +5,17 @@ namespace Refactoring.Pipelines.IntputsAndOutputs
 {
     public static class Inputs1AndOutputs1Extensions
     {
-        public static Inputs1AndOutputs1<TInput1, TOutput1> GetInputAndOutput<TInput1, TOutput1>(this IGraphNode node)
+        public static Inputs<TInput1> GetInputs<TInput1>(this IGraphNode node) { return new Inputs<TInput1>(node); }
+    }
+
+    public class Inputs<T>
+    {
+        private readonly IGraphNode _node;
+        public Inputs(IGraphNode node) { this._node = node; }
+
+        public Inputs1AndOutputs1<T, TOutput1> AndOutputs<TOutput1>()
         {
-            return new Inputs1AndOutputs1<TInput1, TOutput1>(node);
+            return new Inputs1AndOutputs1<T, TOutput1>(this._node);
         }
     }
 
@@ -15,13 +23,9 @@ namespace Refactoring.Pipelines.IntputsAndOutputs
     {
         private readonly InputsAndOutputs _inputsAndOutputs;
 
-        public Inputs1AndOutputs1(IGraphNode node)
-        {
+        public Inputs1AndOutputs1(IGraphNode node) { this._inputsAndOutputs = new InputsAndOutputs(node); }
 
-            this._inputsAndOutputs = new InputsAndOutputs(node);
-        }
-
-        public InputPipe< TInput1> Input
+        public InputPipe<TInput1> Input
         {
             get
             {
@@ -37,13 +41,7 @@ namespace Refactoring.Pipelines.IntputsAndOutputs
             }
         }
 
-        public Tuple<InputPipe<TInput1>, CollectorPipe<TOutput1>> InputsAndOutputs
-        {
-            get
-            {
-                return Tuple.Create(Input, Output);
-            }
-        }
+        public Tuple<InputPipe<TInput1>, CollectorPipe<TOutput1>> AsTuple() { return Tuple.Create(Input, Output); }
 
         public void Send(TInput1 value1) { this.Input.Send(value1); }
     }
