8e4a55fa03c9c690b14e9008701c4ed444bef452
Jay Bazuzi <jbazuzi@tableau.com>
2019-10-05 11:30:12 -0700

    t ApprovalMaintenance_.VerifyNoAbandonedFiles

--- Refactoring.Pipelines.Test/ApprovalTests.Config.cs
+++ Refactoring.Pipelines.Test/ApprovalTests.Config.cs
@@ -1,7 +1,19 @@
-﻿using ApprovalTests.Namers;
+﻿using ApprovalTests.Maintenance;
+using ApprovalTests.Namers;
 using ApprovalTests.Reporters;
 using ApprovalTests.Reporters.TestFrameworks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 [assembly: UseApprovalSubdirectory("_approvals")]
 //[assembly: UseReporter(typeof(MsTestReporter))]
 [assembly: UseReporter(typeof(DiffReporter))]
+
+namespace Refactoring.Pipelines.Test
+{
+    [TestClass]
+    public class ApprovalMaintenance_
+    {
+        [TestMethod]
+        public void VerifyNoAbandonedFiles() { ApprovalMaintenance.VerifyNoAbandonedFiles(); }
+    }
+}
