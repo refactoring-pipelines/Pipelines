144c79c8e52530aea8c3272c24aea614f7ba30c3
Jay Bazuzi <jbazuzi@tableau.com>
2020-01-01 06:11:17 -0800

    F can generate some basics

---
+++ Refactoring.Pipelines.Test/PipelineGenerator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using ApprovalUtilities.Utilities;
+using FluentAssertions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Refactoring.Pipelines.Test
+{
+    public class PipelineGenerator : CSharpSyntaxWalker
+    {
+        private readonly StringBuilder _stringBuilder;
+
+        public PipelineGenerator(StringBuilder stringBuilder) { _stringBuilder = stringBuilder; }
+
+        public override void VisitParameter(ParameterSyntax node)
+        {
+            _stringBuilder.AppendLine("// Parameter: " + node);
+
+            string Generate(ParameterSyntax syntax)
+            {
+                return $@"var {syntax.Identifier}Pipe = new InputPipe<{syntax.Type}>(""{syntax.Identifier}"");";
+            }
+
+            _stringBuilder.AppendLine(Generate(node));
+            _stringBuilder.AppendLine();
+
+            base.VisitParameter(node);
+        }
+
+        public override void VisitInvocationExpression(InvocationExpressionSyntax node)
+        {
+            var arguments = node.ArgumentList.Arguments;
+            if (arguments.Count == 0)
+            {
+                throw new NotImplementedException();
+            }
+
+            var identifierNames = arguments.Select(_ => _.Expression)
+                .Cast<IdentifierNameSyntax>()
+                .Select(_ => _.Identifier.Text);
+
+            this._stringBuilder.Append($"{identifierNames.First()}Pipe");
+
+            if (identifierNames.Count() > 1)
+            {
+                this._stringBuilder.Append(
+                    $".JoinTo({identifierNames.Skip(1).Select(_ => _ + "Pipe").JoinWith(", ")})");
+            }
+
+            var parameterList = "(" + identifierNames.JoinWith(", ") + ")";
+            var expression = node.Expression;
+            this._stringBuilder.Append($".Process({parameterList} => {expression}{parameterList});");
+
+            this._stringBuilder.AppendLine();
+
+
+            base.VisitInvocationExpression(node);
+        }
+
+        public override void VisitLocalDeclarationStatement(LocalDeclarationStatementSyntax node)
+        {
+            _stringBuilder.AppendLine("// " + node);
+            var variableDeclaratorSyntax = node.Declaration.Variables.Single();
+            _stringBuilder.Append($"var {variableDeclaratorSyntax.Identifier}Pipe = ");
+            base.VisitLocalDeclarationStatement(node);
+            _stringBuilder.AppendLine();
+        }
+
+        public static string Generate(string input)
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(input);
+            syntaxTree.GetDiagnostics().Count().Should().Be(0);
+            var rootSyntaxNode = (CompilationUnitSyntax) syntaxTree.GetRoot();
+            var memberDeclarationSyntax = rootSyntaxNode.Members.Single();
+
+            var stringBuilder = new StringBuilder();
+            new PipelineGenerator(stringBuilder).Visit(memberDeclarationSyntax);
+            return stringBuilder.ToString().Trim();
+        }
+    }
+}
---
+++ Refactoring.Pipelines.Test/PipelinesParsingTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using ApprovalTests;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Refactoring.Pipelines.Test
+{
+    [TestClass]
+    public class PipelinesParsingTests
+    {
+        [TestMethod]
+        public void Parameters()
+        {
+            var result = PipelineGenerator.Generate(@"
+Sandwich FindBestSandwich(ZipCode zipCode) { /* ... */ }
+");
+            Approvals.Verify(result);
+        }
+
+        [TestMethod]
+        public void Invocation()
+        {
+            var result = PipelineGenerator.Generate(@"
+                void _() 
+                {
+                    var peanutButters = PeanutButterShop.GetAvailable(zipCode); 
+                }
+");
+            Approvals.Verify(result);
+        }
+
+        [TestMethod]
+        public void Joined()
+        {
+            var result = PipelineGenerator.Generate(@"
+                void _(int value1, int value2)
+                {
+                    var result = F(value1, value2);
+                }
+");
+            Approvals.Verify(result);
+        }
+
+        [TestMethod, Ignore]
+        public void MyTestMethod()
+        {
+            var input = @"
+                Sandwich FindBestSandwich(ZipCode zipCode)
+                {
+                    var peanutButters = PeanutButterShop.GetAvailable(zipCode);
+                    var jellies = JellyShop.GetAvailable(zipCode);
+                    var bestPeanutButter = peanutButters.BestPeanutButter;
+                    var bestJelly = jellies.BestJelly;
+                    var bestSandwich = Sandwich.Create(bestPeanutButter, bestJelly);
+                    return bestSandwich;
+                }
+";
+            var result = PipelineGenerator.Generate(input);
+            Approvals.Verify(result);
+        }
+
+        // force this assembly to load
+        static readonly Type _ = typeof(Microsoft.CodeAnalysis.CSharp.Formatting.CSharpFormattingOptions);
+    }
+}
