fa9440e37c5194aa7fd9a56fc3c2f81a8b8ae912
SimonCropp <simon.cropp@gmail.com>
2020-04-20 08:03:10 +1000

    .

--- Refactoring.Pipelines.Approvals/DotReporter.cs
+++ Refactoring.Pipelines.Approvals/DotReporter.cs
@@ -2,42 +2,8 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using ApprovalTests;
 using ApprovalTests.Core;
-using ApprovalTests.Writers;
-using DiffEngine;
 using EmptyFiles;
-
-namespace Refactoring.Pipelines.ApprovalTests
-{
-    public class PipelineApprovals
-    {
-        static PipelineApprovals()
-        {
-            Extensions.AddTextExtension("dot");
-            var exePaths =new string[]
-            {
-                Environment.ExpandEnvironmentVariables(@"%LocalAppData%\Programs\Microsoft VS Code\code.exe"),
-                Environment.ExpandEnvironmentVariables(@"%LocalAppData%\Programs\Microsoft VS Code\code.exe"),
-            };
-            var exePath = exePaths.FirstOrDefault(File.Exists);
-            if (exePath !=null)
-            {
-                DiffTools.AddCustomTool(DiffTool.VisualStudioCode
-                    buildArguments: (tempFile, targetFile) => { return $"-r \"{tempFile}\""; });
-            }
-        }
-
-        public static void Verify(IGraphNode input)
-        {
-            var dotGraph = DotGraph.DotGraph.FromPipeline(input);
-            Approvals.Verify(WriterFactory.CreateTextWriter(dotGraph.ToString(), "dot"));
-        }
-    }
-}
-
-
-using ApprovalTests.Reporters;
 
 namespace Refactoring.Pipelines.ApprovalTests
 {
