3270dfc5ddcb7692ac15106412fd25e397097d99
Jay Bazuzi <jay@bazuzi.com>
2020-06-06 18:20:05 +0000

    F   graphviz image
    Co-Authored-By: valentiniliescu <valentiniliescu@users.noreply.github.com>
    Co-Authored-By: Olgica Djuric <ondjuric@users.noreply.github.com>

--- Refactoring.Pipelines.ApprovalsWithGraphViz/Class1.cs
+++ Refactoring.Pipelines.ApprovalsWithGraphViz/Class1.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
@@ -18,19 +20,29 @@ namespace Refactoring.Pipelines.ApprovalsWithGraphViz
         public static void VerifyAsPng(IGraphNode input)
         {
             var dotGraph = DotGraph.DotGraph.FromPipeline(input);
+            var graphViz = GetGraphVizGenerator();
+
+            byte[] output = graphViz.GenerateGraph(dotGraph.ToString(), Enums.GraphReturnType.Png);
+            
+            Approvals.VerifyBinaryFile(output, ".png");
+        }
+
+        private static GraphGeneration GetGraphVizGenerator()
+        {
             var getStartProcessQuery = new GetStartProcessQuery();
             var getProcessStartInfoQuery = new GetProcessStartInfoQuery();
             var registerLayoutPluginCommand = new RegisterLayoutPluginCommand(getProcessStartInfoQuery, getStartProcessQuery);
+            var wrapper = new GraphGeneration(getStartProcessQuery, getProcessStartInfoQuery, registerLayoutPluginCommand);
 
-            // GraphGeneration can be injected via the IGraphGeneration interface
+            wrapper.GraphvizPath = Path.Combine(GetNuGetPackagesPath(), "Graphviz.2.38.0.2");
+            return wrapper;
+        }
 
-            var wrapper = new GraphGeneration(getStartProcessQuery,
-                getProcessStartInfoQuery,
-                registerLayoutPluginCommand);
-            wrapper.GraphvizPath = @"C:\source\Pipelines\packages\Graphviz.2.38.0.2";
-
-            byte[] output = wrapper.GenerateGraph(dotGraph.ToString(), Enums.GraphReturnType.Png);
-            Approvals.VerifyBinaryFile(output, ".png");
+        private static string GetNuGetPackagesPath()
+        {
+            var graphVizAssemblyLocation = typeof(GraphVizWrapper.GraphGeneration).Assembly.Location;
+            var packagesFolder = Path.GetDirectoryName(graphVizAssemblyLocation) + @"\..\..\..\..\packages";
+            return Path.GetFullPath(packagesFolder);
         }
     }
 }
