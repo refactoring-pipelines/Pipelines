8f3dbe9e4c37c14ca5a61ca6e0acd3d3e8a28456
Jay Bazuzi <jay@bazuzi.com>
2020-09-19 12:45:41 -0700

    e   Create machine setup script

---
+++ machine-setup.ps1
@@ -0,0 +1,23 @@
+# To run this script directly, open PowerShell and run this command:
+#     PS> Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/refactoring-pipelines/Pipelines/main/machine-setup.ps1 | Invoke-Expression
+
+#Requires -RunAsAdministrator
+
+iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows.ps1 | iex
+iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/visual-studio.ps1 | iex
+
+@(
+    'joaompinto.vscode-graphviz'
+) | % { & "C:\Program Files\Microsoft VS Code\bin\code.cmd" --install-extension $_ }
+
+& "C:\Program Files\Git\cmd\git.exe" clone https://github.com/refactoring-pipelines/Pipelines.git C:\Source\Pipelines
+
+& "C:\Program Files\dotnet\dotnet.exe" test C:\Source\Pipelines\
+
+Write-Host -Foreground yellow " ################################################################"
+Write-Host -Foreground yellow " # Install NuGet API key now:                                   #" 
+Write-Host -Foreground yellow " # 1. Get API key from https://www.nuget.org/account/apikeys    #"
+Write-Host -Foreground yellow " # 2. > NuGet.exe setapikey BLAH-BLAH-BLAH                      #"
+Write-Host -Foreground yellow " ################################################################"
+Write-Host -Foreground yellow ""
+Write-Host -Foreground yellow "Reboot when done"
